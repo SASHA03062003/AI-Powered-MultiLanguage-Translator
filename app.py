f25e4c776b2fb4e5efb548d29b34bb3db35ad422
Sasha Gujrati <gujratisasha@gmail.com>
2026-05-09 16:59:26 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+from deep_translator import GoogleTranslator
+
+app = Flask(__name__)
+
+languages = {
+    "English": "en",
+    "Hindi": "hi",
+    "French": "fr",
+    "German": "de",
+    "Spanish": "es",
+    "Japanese": "ja"
+}
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    translated_text = ""
+
+    if request.method == 'POST':
+
+        text = request.form['text']
+        source_lang = request.form['source_lang']
+        target_lang = request.form['target_lang']
+
+        translated_text = GoogleTranslator(
+            source=source_lang,
+            target=target_lang
+        ).translate(text)
+
+    return render_template(
+        'index.html',
+        translated_text=translated_text,
+        languages=languages
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
